efd7bb86ec5cb923c4a50372382f81fcb9a50df1
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-02-12 18:21:30 -0400

    Creamos nuestro contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_VladimirGuzman.Models;
+
+namespace Parcial1_Ap1_VladimirGuzman.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Metas> Metas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+    }
+}

0e692ba5bbae40a49b5e52e9b58526341c143385
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-02-12 18:18:15 -0400

    Creando modelo Metas

---
+++ Models/Metas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_VladimirGuzman.Models
+{
+    public class Metas
+    {
+        [Key]
+        public int MetaId { get; set; }
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage ="Debes espedificar cual es tu menta")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage ="Desbes ingresar el monto")]
+        [Range(1, int.MaxValue, ErrorMessage ="No puede ser un monto mayor que 0")]
+        public int Monto { get; set; }
+    }
+}

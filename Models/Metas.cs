a55c9f4e172ea13071e9451d709bd85d7b982102
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-02-20 23:47:42 -0400

    Corregimos una falta ortografica y eliminamos punto y coma en lugares innecesarios

--- Models/Metas.cs
+++ Models/Metas.cs
@@ -7,7 +7,7 @@ namespace Parcial1_Ap1_VladimirGuzman.Models
         [Key]
         public int MetaId { get; set; }
         public DateTime Fecha { get; set; }
-        [Required(ErrorMessage ="Debes espedificar cual es tu menta")]
+        [Required(ErrorMessage ="Debes espesificar cual es tu menta")]
         public string? Descripcion { get; set; }
         [Required(ErrorMessage ="Desbes ingresar el monto")]
         [Range(1, int.MaxValue, ErrorMessage ="No puede ser un monto Menor que 0")]

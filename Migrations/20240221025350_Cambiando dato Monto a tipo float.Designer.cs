b12474fa76b2d34aaf761316f57bcd417cf4b455
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-02-20 22:55:46 -0400

    Modificamos dato Monto a tipo float

--- Migrations/20240221025350_Cambiando dato Monto a tipo float.Designer.cs
+++ Migrations/20240221025350_Cambiando dato Monto a tipo float.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parcial1_Ap1_VladimirGuzman.DAL;
 
@@ -10,9 +11,11 @@ using Parcial1_Ap1_VladimirGuzman.DAL;
 namespace Parcial1_Ap1_VladimirGuzman.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240221025350_Cambiando dato Monto a tipo float")]
+    partial class CambiandodatoMontoatipofloat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
--- Models/Metas.cs
+++ Models/Metas.cs
@@ -11,6 +11,6 @@ namespace Parcial1_Ap1_VladimirGuzman.Models
         public string? Descripcion { get; set; }
         [Required(ErrorMessage ="Desbes ingresar el monto")]
         [Range(1, int.MaxValue, ErrorMessage ="No puede ser un monto Menor que 0")]
-        public int Monto { get; set; }
+        public float Monto { get; set; }
     }
 }
